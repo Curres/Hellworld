8ab8829e6f4ab649a631cfae51188d0073356a2d
soft01 <soft01@23.5.ftth.classcom.pl>
2019-08-15 08:39:29 +0800

    test

---
+++ src/list/Demo.java
@@ -0,0 +1,12 @@
+package list;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		System.out.println("巨野");
+		test();
+	}
+	public static  void test() {
+		System.out.println("测试");
+	}
+}
